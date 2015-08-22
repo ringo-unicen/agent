a2d1cc284be69f5f8c331f6ac37e4ed16d8a3e57
Pablo Saavedra <psaavedra@devspark.com>
2015-08-22 12:51:09 -0300

    Completed start and stop functionality, needs integration testing now

--- modules/router.js
+++ modules/router.js
@@ -1,27 +1,27 @@
 /// <reference path="../typings/lodash/lodash.d.ts"/>
-var os = require('os');
 var _ = require('lodash');
 var iptabler = require('iptabler');
+var Bluebird = require('bluebird');
 
-var ifaces= os.networkInterfaces();
-var ip = _.chain(ifaces).map(_.identity).flatten().filter({internal: false, family: 'IPv4'}).first().value();
 
-console.log('Using IP', ip);
-
-exports.address = ip.address;
-
-var config = {
-    active: false
+var getIp = function () {
+    var ifaces= require('os').networkInterfaces();
+    console.log('Scanning for IP address in', ifaces);
+    var ip = _.chain(ifaces).map(_.identity).flatten().filter({internal: false, family: 'IPv4'}).first().value();
+    console.log('Using IP', ip);
+    return ip;
 };
 
-var applied = false;
+var config = {
+    state: 'STOPPED'
+};
 
 
-var update = function () {
+var forward = function () {
     var preroute = iptabler();
     preroute
         .t('nat')
-        .a('PREROUTING')
+        .A('PREROUTING')
         .p('tcp')
         .dport(config.target.port)
         .j('DNAT')
@@ -32,16 +32,43 @@ var update = function () {
      var posroute = iptabler();
      posroute
         .t('nat')
-        .a('POSTROUTING')
+        .A('POSTROUTING')
         .p('tcp')
         .dport(config.target.port)
         .d(config.target.ip)
         .j('SNAT')
-        .toSource(ip.address);
+        .toSource(getIp().address);
      console.log(posroute._args);
-     applied = true;
+     return Bluebird.resolve("OK");
 
+     //TODO Rollback in case of error and report
     // Example: 
     //    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.12.77:80
     //    iptables -t nat -A POSTROUTING -p tcp -d 192.168.12.77 --dport 80 -j SNAT --to-source 192.168.12.87
-}
+};
+
+var serve = function () {
+    console.log('Resetting all rules to default');
+    var flush = iptabler().t('nat').F();
+    console.log(flush._args);
+    return Bluebird.resolve('OK');
+};
+
+
+exports.configure = function (newConfig) {
+    config.target = newConfig.next;
+};
+
+exports.start = function () {
+    if (_.isUndefined(config.target)) {
+        return Bluebird.reject(new Error('Target is undefined, configure the router first'));
+    }
+    return serve();
+};
+
+exports.stop = function () {
+    if (_.isUndefined(config.target)) {
+        return Bluebird.reject(new Error('Target is undefined, configure the router first'));
+    }
+    return forward();
+};
--- routes/agent.js
+++ routes/agent.js
@@ -1,21 +1,41 @@
 module.exports = function (app) {
-    var address = require('../modules/router').address;
-    console.log('IP:', address);
+    var _ = require('lodash');
+    var router = require('../modules/router');
+
     app.route('/start')
         .post(function (req, res) {
             console.log('Starting agent');
-            res.json(req.body);
+            router.start().then(function (result) {
+                res.json({msg: result});
+            }).catch(function (err) {
+                console.log('Error starting agent', err);
+                res.json({msg: err.message});
+            });
         });
 
     app.route('/config')
         .post(function (req, res) {
            console.log('Configuring agent with', req.body);
-           res.json(req.body); 
+           if (_.isUndefined(_.get(req.body, 'next.ip'))) {
+               res.status(400).json({msg: 'next.ip is required'});
+               return;
+           }
+           if (_.isUndefined(_.get(req.body, 'next.port'))) {
+               res.status(400).json({msg: 'next.port is required'});
+               return;
+           }
+           router.configure(req.body);
+           res.json({msg: 'Successfully updated configuration'});
         });
 
     app.route('/stop')
         .post(function (req, res) {
             console.log('Stopping agent');
-            res.json(req.body);
+            router.stop().then(function (result) {
+                res.json({msg: result});
+            }).catch(function (err) {
+                console.log('Error stopping agent', err);
+                res.json({msg: err.message});
+            });
         });
 };
