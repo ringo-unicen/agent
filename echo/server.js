161695f6542ecebd09865db82af74d4fc06dbab1
Pablo Saavedra <psaavedra@devspark.com>
2015-09-12 11:47:42 -0300

    added an echo server for testing purposes

---
+++ echo/server.js
@@ -0,0 +1,25 @@
+/// <reference path="../typings/node/node.d.ts"/>
+var http = require('http');
+
+
+console.log('Starting server');
+var echo = function(req, res) {
+    console.log('Handling request', req);
+    var body = '';
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function() {
+        res.writeHead(200, 'OK', {'Content-type': 'text/plain'});
+        res.write(req.method + ' ' + req.url);
+        res.write('\n');
+        res.write(body);
+        res.write('\n');
+        res.end();
+        console.log('Successfully handled request');
+    });
+};
+
+var port = process.env.PORT || 8080;
+http.createServer(echo).listen(port);
+
